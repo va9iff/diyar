d3c092895b1f5fee6d18f3c12a7449d42c29b629
va9iff <va9iff@gmail.com>
2024-11-11 01:47:04 +0400

    no neighbours get to be whole road; add win function

--- modes/shortestPathCities.js
+++ modes/shortestPathCities.js
@@ -14,7 +14,7 @@ const m = {
 	tried: 0,
 	ideal: 1,
 	selecteds: [],
-	adjecents: [],
+	shortestRoad: [],
 	wents : ["Abşeron"]
 }
 
@@ -26,6 +26,9 @@ function checkWents() {
 		}
 	}
 }
+function win() {
+	alert("Win!")
+}
 
 const inputArg  = {
 	list: pathTitles,
@@ -33,15 +36,16 @@ const inputArg  = {
 		if (m.selecteds.includes(city)) return null
 		clear("#eee")
 		m.activeCity = city
-		m.adjecents = shortest(m.activeCity, m.to)
+		m.shortestRoad = shortest(m.activeCity, m.to)
 		m.selecteds.push(city)
 		for (const s of m.selecteds) fill(s, "#ff7")
 		checkWents()
+		fill(m.activeCity, "#f33")
 		for (const w of m.wents) fill(w, "#7ff")
 		fill(m.from, "#7f7")
 		fill(m.to, "#77f")
-		fill(m.activeCity, "#f33")
 		m.tried++
+		if (m.wents.some(went => neighbours[went].includes(m.to))) win()
 	}
 }
 
@@ -50,22 +54,21 @@ export const shortestPathCities = {
 		const cachedRandomCity = randomCity()
 		Object.assign(m, {
 			from: cachedRandomCity,
-			to: randomCity(cachedRandomCity),
+			to: randomCity(cachedRandomCity, neighbours[cachedRandomCity]),
 			selecteds: [],
 		})
-		m.adjecents=shortest(m.from, m.to)
+		m.shortestRoad=shortest(m.from, m.to)
 		m.activeCity = m.from
 		m.wents = [m.from]
 		m.tried = 0
-		console.log(m.adjecents)
-		m.ideal = m.adjecents.length - 1
+		m.ideal = m.shortestRoad.length - 1
 		inputArg.list = pathTitles.filter(t => ![m.from, m.to].includes(t))
-		inputArg.activeChip = -1
+		inputArg.activeChip = -2
 
 		clear("#eee")
 		fill(m.from, "#7f7")
 		fill(m.to, "#77f")
-		fill(m.activeCity, "#f33")
+		// fill(m.activeCity, "#f33")
 
 	},
 	content() {
