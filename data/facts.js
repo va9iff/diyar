ecbf2a377d389a2c45916a0d08c37dc06eafe59f
va9iff <va9iff@gmail.com>
2024-12-11 17:40:10 +0400

    actual fix

--- data/facts.js
+++ data/facts.js
@@ -7,45 +7,45 @@ export const facts = {
         `İl ərzində çox böyük yağıntı alan bu rayon "Hikran milli parkını"-nın böyük hissəsini əhatə edir.`,
         `1963-cü ildə ləğv olunaraq Lənkərana birləşdirilsə də, 1965-ci ildə yenidən müstəqil olmuşdur.`,
     ],
-    "Ağcabədi ": [
+    "Ağcabədi": [
         `Qarqar çayının hər iki sahilində, həm Mil, həm də Qarabağ düzlərində yerləşən rayonumuz hansıdır?`,
         `Bu rayon 1963-cü ildə ləğv olunub Ağdamın tabeliyinə verilmiş, 1965-ci ildə isə yenidən  müstəqil olmuşdur.`,
     ],
-    "Ağdam ": [
+    "Ağdam": [
         `Bu rayonda 1983-cü ildə istifadəyə verilən "Çörək zavodu" ermənilərin atəşlərinə tab gətirməyərək 1992-ci ildə yararsız vəziyyətə düşmüşdür.`,
         `"Köhlən atlar" adlandırılan futbol klubu məhz bu rayonu təmsil edir.`,
     ],
-    "Ağdaş ": [
+    "Ağdaş": [
         `Pambıq istehsalının çox geniş yayılmasının nəticəsi olaraq Qafqaz pambıqçılarının ilk qurultayının 1904-cü ildə burada keçirilmişdi.`,
     ],
     "Ağstafa": [
         `Burada yerləşən "Qarayazı dövlət təbiət qoruğu" unikal flora və faunası ilə seçilir.`,
         `Unikal təbii abidə hesab olunan Göyəzən dağı bu rayonun sərhədləri daxilindədir.`,
     ],
-    "Ağsu ": [
+    "Ağsu": [
         `1734-cü ildə Nadir şah tərəfindən Şamaxı rayonunun əhalisi o vaxt "Yeni Şamaxı adlanan bu əraziyə köçürüldü.`,
         `Bu rayonunun Gəgəli kəndində yerləşən bu türbə unikal memarlıq üslubuna malikdir və Azərbaycan tarixi abidələri sırasında özünəməxsus yer tutur.`,
     ],
-    "Babək ": [
+    "Babək": [
         `Əfsanəyə görə Nuh peyğəmbərin gəmisi bu rayon ərazisində-Gilançayın yuxarı axarındakı Gəmiqayada lövbər salmışdır.`,
         `Adını IX əsrdə ərəb xilafətinə qarşı olan "Xürrəmilər" hərəkatının ən uğurlu liderindən almışdır.`,
     ],
-    "Bakı ": [
+    "Bakı": [
         `Bu inzibati rayonun adının etimalogiyasına əsasən onun mənası ərəb dilindən gələn “Bad Küpe” - “küləklər şəhəri” deməkdir.`,
         `Buradakı eyniadlı şəhər keçmişdə Şirvanşahlar, indi isə Azərbaycan Respublikasının paytaxtıdır.`,
     ],
-    "Balakən ": [
+    "Balakən": [
         `Bu rayonun ərazisindən Mazımçay, Alazançay və s. çaylar axır.`,
         `Filizçay və Katexçay polimetal yataqları bu rayonda yerləşir.`,
     ],
-    "Beyləqan ": [
+    "Beyləqan": [
         `Günümüzdə İranla sərhəddə yerləşən bu rayon 1227-ci ildə Xarəzmşahlar tərəfindən darmadağın edilmişdir.`,
     ],
-    "Biləsuvar ": [
+    "Biləsuvar": [
         `Bu rayon 1991-ci ilədək Puşkin rayonu adlandırımışdı.`,
         `Azərbaycanın ən böyük çaylarından biri olan Bolqarçay bu rayon ərazisindən axır.`,
     ],
-    "Bərdə ": [
+    "Bərdə": [
         `Bu ərazidə yerləşən eyniadlı şəhər "Arranın paytaxtı adlanırdı. (Söhbət Qəbələdən getmir)`,
         `944-cü ildə slavyanların hücumu ilə məhv edilən bu rayon tarix boyu bir daha əvvəlki kimi güclü ola bilmədi.`,
     ],
@@ -53,19 +53,19 @@ export const facts = {
         `Buradakı eyniadlı şəhər Naxçıvan Muxtar Respublikasının ən böyük şəhəridir.`,
         `İran ərazisində, Araz çayının əks sahilində bu şəhərə simmetrik şəkildə eyniadlı şəhər yerləşir.`,
     ],
-    "Cəbrayıl ": [
+    "Cəbrayıl": [
         `2016-cı ilin Aprel döyüşlərində işğaldan azad edilən "Lələtəpə yüksəkliyi” bu rayonun ərazisində yerləşir.`,
         `Bu rayon 2020-ci ilin 4 oktyabr Azərbaycanın Silahlı Qüvvələri tərəfindən işğaldan azad edilib və hazırda bərpa və yenidənqurma işləri aparılır.`,
     ],
-    "Cəlilabad ": [
+    "Cəlilabad": [
         `1964-cü ildə Biləsuvar rayonunun ərazisinə birləşdirilsə də, 1965-ci ildə yenidən müstəqil olmuşdur.`,
         `1967-ci ildən etibarən görkəmli yazıçı-dramaturq Cəlil Məmmədquluzadənin şərəfinə indiki adı ilə adlandırılıb.`,
     ],
-    "Daşkəsən ": [
+    "Daşkəsən": [
         `Hinaldağ  və Qoşqar zirvələrinin yerləşdiyi rayonumuz hansıdır.`,
         `Zəylik alunit yatağını məhz bu rayonda yerləşir.`,
     ],
-    "Füzuli ": [
+    "Füzuli": [
         `Bu rayondakı eyniadlı şəhər 17 oktyabr 2020-ci ildə erməni işğalından azad olunmuşdur.`,
         `Quruçay mədəniyyəti deyilən tarixi ərazi bu rayonumuzun ərazisindədir.`,
     ],
@@ -73,37 +73,37 @@ export const facts = {
         `Kiçik Qafqazda yerləşən bu rayonumuzda Acıdərə və Qazanbulaq neft yatağları yerləşir.`,
         `Korçay yasaqlığı məhz bu rayon ərazisində yerləşir.`,
     ],
-    "Göygöl ": [
+    "Göygöl": [
         `Çar Rusiyası dövründə kəndli almanlar məhz bu rayon ərazisindı olan Yelenendorf yaşayış məntəqəsinə köçürülmüşdülər.`,
         `1139-cu ildə baş verən zəlzələ nəticəsində Ağsu çayının qarşısının kəsilməsi nəticəsində yaranan göl burada yerləşir.`,
     ],
-    "Göyçay ": [
+    "Göyçay": [
         `Bu rayonun adı əsl türk sözü olub, "mavi çay" deməkdir.`,
         `Bu rayonda hər ilin oktyabr və noyabr aylarında nar festivalı keçirilir.`,
     ],
-    "Gədəbəy ": [
+    "Gədəbəy": [
         `Kartofçuluq deyəndə ilk ağla gələn Azərbaycan rayonu məhz buradır.`,
         `Bu rayon 19-cu əsrin ikinci yarısında burada Alman şirkətləri tərəfindən misəritmə zavodlarınının əsası qoyulmuşdu.`,
     ],
-    "Gəncə ": [
+    "Gəncə": [
         ``,
     ],
-    "Hacıqabul ": [
+    "Hacıqabul": [
         `Pirqarın, Göytəpə, İncəbel və digər palçıq vulkanları ilə zəngindir.`,
         `Hülakilər dövründə Mahmudabad sonrakı dövrlərdə Həcc ziyarəti ilə əlaqədar indiki adını daşımağa başlamışdır.`,
     ],
-    "Kürdəmir ": [
+    "Kürdəmir": [
         `Bu rayonun adının etimalogiyasının Kür çayının daşarkən suyun çatmadığı (dəymədiyi) ərazi sözündən yarandığı güman edilir.`,
         `Azərbaycanın ən isti yeri dedikdə ağla ilk olaraq bu ərazi gəlir.`,
     ],
-    "Kəlbəcər ": [
+    "Kəlbəcər": [
         `Söyüdlü-Zod qızıl yatağı bu rayon ərazisində yerləşir.`,
         `Bu rayon 2023-cü ilədək Azərbaycanın ən böyük rayonu adını daşıyırdı.`,
     ],
-    "Kəngərli ": [
+    "Kəngərli": [
         `Bərdə və Şərur rayonları arasında yerləşən bu rayon peçeneqlərin qədim vətəni olmuşdur.`,
     ],
-    "Laçın ": [
+    "Laçın": [
         `Vətən müharibəsi zamanı məhz bu rayonda yerləşən dəhlizin Azərbaycan ordusunun nəzarətinə keçməsi ermənilərin Ermənistanla olan əlaqələtini kəsdi.`,
         `Qarabağın general qubernatoru olan Xosrov bəy Sultanov məhz bu rayonda dünyaya gözlərini açmışdır.`,
     ],
@@ -111,19 +111,19 @@ export const facts = {
         `Bu rayonun ən yüksək zirvələri cənub silsiləsindəki Gömürgöy (2493 m) və Qızyurdudur (2433 m).`,
         `Rayonun Zuvand ərazisində Zuvand dövlət yasaqlığı (qoruq) vardır.`,
     ],
-    "Lənkəran ": [
+    "Lənkəran": [
         `Yüksək yağıntıya görə tez-tez daşan çayların qarçısını almaq üçün Xanbulan su anbarı burada tikilmişdir.`,
         `Nadir flora və faunası ilə seçilən Hirkan Milli parkı bu rayon ərazisində yerləşir.`,
     ],
-    "Masallı ": [
+    "Masallı": [
         `Bəzi mənbələr bu rayonun adının "Mosullu" sözündən, bəziləri isə Masal xanın şərəfinə belə adlandırıldığıdı deyir.`,
         `Talışların sayca üstünlük təşkil etdiyi bu rayonda bir neçə termal su mənbəsi var ki, bunlardan biri də "İstisu"-dur.`,
     ],
-    "Mingəçevir ": [
+    "Mingəçevir": [
         ``,
     ],
-    "Naxçıvan ": [],
-    "Neftçala ": [
+    "Naxçıvan": [],
+    "Neftçala": [
         `Həm palçıq vulkanları, həm də neftlə bol olan bu rayonda Azırbaycanın ilk neft mədənlərindən biri olan Qarasu neft yatağı yerləşir.`,
         `Kür çayı Xəzər dənizinə məhz bu ərazidə tökülür.`,
     ],
@@ -131,7 +131,7 @@ export const facts = {
         `Quru meyvələr dedikdə ilk ağla gələn yer məhz bu rayondur.`,
         `Bu rayonun əhalisi 1603-cü ildə Səfəvilərə yardımlna görə vergidən azad olunmuşdur.`,
     ],
-    "Oğuz ": [
+    "Oğuz": [
         `Bu rayonda alban məbədlərinin, Muxas qülləsinin (XIV əsr), Surxayxan qalasının qalıqları (XVIII əsr) bu günə kimi qalmışdır.`,
         `Türklər, ləzgilər, avarlar və digər etnik azlıqların bir yerdə yaşadığı nu rayonda olan Nohurqışlaq gölü turistlərin diqqətini cəlb edir.`,
     ],
@@ -151,7 +151,7 @@ export const facts = {
         `Türk mənşəli massaget tayfalarının yaşadığı bu rayon günümüzdə dadlı almaları ilə seçilir.`,
         `Peyğəmbərimizin Mədinə şəhəri ətrafında tikdiyi ilk məscidin adı belə adlanır.`,
     ],
-    "Qubadlı ": [
+    "Qubadlı": [
         `1921-ci ildə rus-erməni, eləcə də daşnak-bolşevik ittifaqının məntiqi nəticəsi olaraq, imperiya mahalı iki yerə bölmüşdür.`,
         `25 oktyabr 2020-ci ilədək mənfur düşmənin işğalı altında olan bu şəhərin adı adı Şirvanşah Şeyx İbrahim Dərbəndinin babasının adı KeyQubadın adı ilə bağlıdır`,
     ],
@@ -159,38 +159,38 @@ export const facts = {
         `Bu rayon mövqeyinə gürə Azərbaycanın şimal qapısı adlanır.`,
         `Azərbaycanın ən hündür zirvəsi olan Bazardüzü ləzgilərin say çoxluğu ilə seçilən bu rayonda yerləşir.`,
     ],
-    "Qəbələ ": [
+    "Qəbələ": [
         `1991-ci ilədək bu rayon Qutqaşen adlandırılırdı.`,
         `Bu şəhər V əsrə qədər Albaniya dövlətinin paytaxtı olub.`,
     ],
-    "Şabran ": [
+    "Şabran": [
         `Məşhur Qalaaltı kurortu bu rayonun ərazisindədir.`,
         `Bu rayonun keşmiş adı Dəvəçidir.`,
     ],
-    "Şahbuz ": [
+    "Şahbuz": [
         `Batabat gölü və Badamlı istirahət mərkəzi bu ərazidə yerləşir.`,
         `Əlincə qalası həm Ermənistan, həm də İranla yerləşən bu rayonda yerləşir.`,
     ],
-    "Şamaxı ": [
+    "Şamaxı": [
         `Şirvanşahlar dövlətinin 2-ci paytaxtı bu rayonla eyniadlı şəhər olmuşdur.`,
         `Azərbaycandakı ən böyük rəsədxana bu rayon ərazisində yerləşir.`,
     ],
-    "Şuşa ": [
+    "Şuşa": [
         `Cıdır düzünün də yerləşdiyi bu rayonun eyni adlı şəhərinin 8 noyabr 2020-ci ildə azad olunmasına görə hər il eyni tarix "Zəfər günü" kimi qeyd olunur.`,
         `Azərbaycanın mədəniyyət mərkəzi və muğamın beşiyi kimi tanınan bu rayon bir çox tarixi abidələrə də ev sahibliyi edir.`,
     ],
-    "Şəki ": [
+    "Şəki": [
         `Əvvəllər Nuxa adlandırılan bu rayon ipəkçiliyin ən çox inkişaf etdiyi ərazilərdən biridir.`,
         `Burada 18-ci əsrdə inşa edilən xan sarayı rəngli şüşələrdən yığılmış şəbəkə-pəncərələri ilə məşhurdur.`,
     ],
-    "Şəmkir ": [
+    "Şəmkir": [
         `1818-ci ildə bura Çar Rusiyası tərəfindən gətirilən almanlar hal-hazırda Göygölün ərazisi olan Annenfeld və Yelenendorfda məskunlaşdırılmışdılar.`,
     ],
-    "Şərur ": [
+    "Şərur": [
         `Bu rayon Sədərəyin yeganə qonşusudur.`,
         `Bu rayonda yerləşən "Ağkörpü" və "Qaragöl" kimi arxeoloji abidələr bu sahənin mühüm tədqiqat obyektləridir.`,
     ],
-    "Saatlı ": [
+    "Saatlı": [
         `Bu rayon Əmir Səədin adını daşıyan və Ağrıdağ vadisində yaşayan türk mənşəli Saadlı tayfasının adı ilə adlandırılıb.`,
     ],
     "Sabirabad": [
@@ -205,50 +205,50 @@ export const facts = {
         `Eldar Şamı Dövlət qoruğu məhz bu rayonda yerləşir.`,
         `Bu rayonun ərazisindən Kür, Qabırrıçay (İori), Alazan (Qanıx), Gəncəçay və Qoşqarçay çayları keçir.`,
     ],
-    "Siyəzən ": [
+    "Siyəzən": [
         `Pir Xəlil Türbəsi bu rayonun ərazisində yerləşir.`,
     ],
-    "Sumqayıt ": [
+    "Sumqayıt": [
         ``,
     ],
-    "Sədərək ": [
+    "Sədərək": [
         `Bu rayon həm Ermənistan, həm Türkiyə, həm də İranla sərhəddə yerləşir.`,
         `Türkiyə ilə yeganə quru sərhədimiz olan bu rayonda Əjdahan mağarası yerləşir.`,
     ],
     "Tovuz": [
         `2020-ci ilin iyulunda döyüş zamanı General Polad Həşimov bu rayonda şəhid olmuşdur.`,
     ],
-    "Tərtər ": [
+    "Tərtər": [
         `Bu rayonun keşmiş adı Mirbəşirdir.`,
     ],
     "Ucar": [
         `Əfsanəyə görə bu rayon adını "Ucqar" sözündən götürüb; belə ki, bura ilə Şirvanşahların paytaxtı arasında uzun məsaf var idi.`,
     ],
-    "Xaçmaz ": [
+    "Xaçmaz": [
         `Pomidor istehsalına görə Azərbaycan ən irəlidə olan rayondur.`,
         `1963-cü ildə bu rayon ləğv edilib, ərazisi Quba və Qusar rayonlarına verilmişdir, 1965-ci ildə rayon yenidən bərpa edilmişdir.`,
     ],
-    "Xocalı ": [
+    "Xocalı": [
         `Bu rayon 19 sentyabr 2023-cü ildə keçirilən lokal xarakterli antiterror əməliyyatı nəticəsində işğaldan azad olunmuşdur və 24 sentyabrda Azərbaycan ordusunun nəzarətinə keçmişdir.`,
         `1992-ci il 26 fevral tarixində dünya tarixinin ən böyük soyqırımlarından biri məz bu rayonda baş vermişdi.`,
     ],
-    "Xocavənd ": [
+    "Xocavənd": [
         `Əhəng daşları oksfordkimeric dövrünə aid olan Tağlar mağarası məhz bu rayonda yerləşir.`,
         `Rayon mərkəzi 2 oktyabr 1992-ci il tarixində Ermənistan Silahlı Qüvvələrinın tərəfindən işğal edilmişdir.`,
     ],
-    "Xızı ": [
+    "Xızı": [
         `Bu rayonun ərazisində təbiətin yaratdığı ən ifadəli abidələrdən biri – ecazkar formalı Beşbarmaq qayası yerləşir.`,
         `Bu rayon Sasanilər dövründən başlayan köçürmə siyasətinin nəticəsi olaraq bura "Xuzistan" əyalətindən gətirilən tayfalardan alıb.`,
     ],
-    "İmişli ": [
+    "İmişli": [
         `Qızılkənd kəndində Tunc və Dəmir dövrlərinə aid Yedditəpə kurqanı bu rayonun ərazisindədir.`,
         `Bu rayonda Cənubi Qafqazın əb böyük şəkər zavodu yerləşir.`,
     ],
-    "İsmayıllı ": [
+    "İsmayıllı": [
         `Qovdağ və Niyaldağ silsilələri bu rayon ərazisində yerləşir.`,
         `Cavanşirin mənsub olunduğu Mehranilər Girdiman knyazlığını məhz bu ərazidə yaratmışdır.`,
     ],
-    "Yardımlı ": [
+    "Yardımlı": [
         `İranla həmsərhəd olan bu rayon ərazisindən axan ən böyük çay Viləşçaydır.`,
     ],
     "Yevlax": [
@@ -259,15 +259,15 @@ export const facts = {
         `Bu rayonun adının etimalogiyası türk mənşəlisak tayfalarından gəlir; "Sak düzü" və ya "Sak tala" birləşmələrindən yaranmışdır.`,
         `Bu rayonun ən uca zirvəsi Qutondağdır. (3648 metr)`,
     ],
-    "Zəngilan ": [
+    "Zəngilan": [
         `Bu rayondakı çinar meşəsi Avropada ən böyük, dünyada isə Kanadadan sonra ikincidir.`,
         `Vejnəli qızıl yatağı yerləşən by rayonun mərkəzi şəhəri 20 oktyabr 2020-ci ildə işğaldan azad olunmuşdur.`,
     ],
-    "Zəngəzur ": [
+    "Zəngəzur": [
         `bu tarixi azərbaycan torpaqları cavidan, cavanşir və babəkin dövründə xürrəmilər, ərəblərə qarşı savaşların əsas mərkəzi, 30 ildən artıq sürən döyüşlərin məkanı olub.`,
         `2020-ci il 10 noyabrda imzalanan üçtərəfli bıyanata əsasən buradan keçən dəhliz naxçıvan ilə azərbaycanı quru yolla bağlayacaq.`,
     ],
-    "Zərdab ": [
+    "Zərdab": [
         `Bu rayon 1963-cü ildə ləğv olunub Ucarın tabeliyinə verilmiş, 1965-ci ildə isə yenidən  müstəqil olmuşdur.`,
 	]
 }
